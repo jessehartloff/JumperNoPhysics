0ee02a33772c846b39f46171857e4c6dc6165678
jessehartloff <hartloff@buffalo.edu>
2020-09-26 16:26:39 -0400

    Change magnitude to component x, y, z can be negative and therefore contain directional information, not just magnitude

--- src/physics/PhysicsVector.scala
+++ src/physics/PhysicsVector.scala
@@ -4,9 +4,9 @@ package physics
   * A 3-Dimension vector in the physical space. The x/y plane is parallel to the ground and the z-axis is perpendicular
   * to the ground. Gravity is applied in the negative z direction (ie. positive z is up)
   *
-  * @param x magnitude of the vector in the x direction
-  * @param y magnitude of the vector in the y direction
-  * @param z magnitude of the vector in the z direction
+  * @param x-axis component of the vector
+  * @param y-axis component of the vector
+  * @param z-axis component of the vector
   */
 class PhysicsVector(var x: Double = 0.0, var y: Double = 0.0, var z: Double = 0.0) {
 
